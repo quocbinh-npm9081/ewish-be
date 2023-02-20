fe49df5a2cc579cadbccd7edd402950effd8fd87
louis <quocbinhgithub@gmail.com>
2023-02-20 21:54:52 +0700

    feat: add register and login controller

--- src/controllers/Auth.controllers.ts
+++ src/controllers/Auth.controllers.ts
@@ -1,12 +1,40 @@
 import { Request, Response } from 'express';
-
+import User from '@/models/User';
+import bcrypt from 'bcrypt';
 class AuthControllers {
-  public register(req: Request, res: Response) {
+  public async register(req: Request, res: Response) {
     const { username, email, password } = req.body;
     if (!username || !email || !password) {
       res.status(200).json({ msg: 'VUi long nhap day du' });
     }
-    res.status(200).json({ msg: 'dang ki thanh cong' });
+    const passwordHashed = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username: username,
+      email: email,
+      password: passwordHashed,
+    });
+    try {
+      const savedUser = await newUser.save();
+      res.status(201).json({ msg: 'dang ki thanh cong', user: savedUser });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ msg: error });
+    }
+  }
+
+  public async login(req: Request, res: Response) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) res.status(201).json({ msg: 'Nguoi dung khong ton tai' });
+      else {
+        const isPassword = await bcrypt.compare(password, user?.password);
+        if (isPassword) res.status(201).json({ msg: 'Dang nhap thanh cong' });
+        else res.status(201).json({ msg: 'sai mat khau' });
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
--- src/routes/Auth.routes.ts
+++ src/routes/Auth.routes.ts
@@ -3,5 +3,6 @@ import AuthControllers from '@/controllers/Auth.controllers';
 const router = Router();
 
 router.post('/register', AuthControllers.register);
+router.post('/login', AuthControllers.login);
 
 export default router;
